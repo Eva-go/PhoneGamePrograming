301a297fa1199a6ec34f28e2d31357ca75ff8672
Jo <jkyu6554@kpu.ac.kr>
2021-03-22 12:02:33 +0900

    editTextView 만들기

--- Programing/MoreControls/app/src/main/java/kr/ac/kpu/game/s2016182041/morecontrols/MainActivity.java
+++ Programing/MoreControls/app/src/main/java/kr/ac/kpu/game/s2016182041/morecontrols/MainActivity.java
@@ -11,19 +11,25 @@ public class MainActivity extends AppCompatActivity {
 
     private CheckBox firewallCheckbox;
     private TextView outTextView;
-
+    private TextView userEditText;
+    private TextView editTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         firewallCheckbox = findViewById(R.id.checkbox);
         outTextView =findViewById(R.id.outTextView);
+        userEditText =findViewById(R.id.editText);
+        editTextView =findViewById(R.id.editTextView);
     }
 
     public void onBtnApply(View view) {
         boolean checked = firewallCheckbox.isChecked();
         String text=checked ? "Using Firewall":"Not using Firewall";
         outTextView.setText(text);
+
+       String user = userEditText.getText().toString();
+       editTextView.setText("User info =" +user);
     }
 
     public void onCheckFirewall(View view) {
