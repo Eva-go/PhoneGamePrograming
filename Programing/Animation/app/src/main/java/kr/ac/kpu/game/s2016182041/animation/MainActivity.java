170c7ee5a0bef62bee54f828a4b8ad19a637682c
Jo <jkyu6554@kpu.ac.kr>
2021-03-29 10:47:42 +0900

    orientation change add

--- Programing/Animation/app/src/main/java/kr/ac/kpu/game/s2016182041/animation/MainActivity.java
+++ Programing/Animation/app/src/main/java/kr/ac/kpu/game/s2016182041/animation/MainActivity.java
@@ -1,19 +1,31 @@
 package kr.ac.kpu.game.s2016182041.animation;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
-import android.view.MotionEvent;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //MyView mv = new MyView(this);
+        Log.d(TAG, "onCreate");
+//        MyView mv = new MyView(this);
         //커스텀 뷰 제작
-        //setContentView(mv);
+//        setContentView(mv);
+    }
+
+    @Override
+    public void onConfigurationChanged(@NonNull Configuration newConfig) {
+        Log.d(TAG, "onConfigChange:" + newConfig);
+        super.onConfigurationChanged(newConfig);
     }
 }
+
