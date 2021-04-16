4abfe149051300b935a080e25e369973690a9b67
Jo <jkyu6554@kpu.ac.kr>
2021-04-16 11:03:52 +0900

    load()를 pull - up

--- Programing/SampleGame/app/src/main/java/kr/ac/kpu/game/s2016182041/samplegame/framework/AnimationGameBitmap.java
+++ Programing/SampleGame/app/src/main/java/kr/ac/kpu/game/s2016182041/samplegame/framework/AnimationGameBitmap.java
@@ -1,15 +1,10 @@
 package kr.ac.kpu.game.s2016182041.samplegame.framework;
 
-import android.content.res.Resources;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Rect;
 import android.graphics.RectF;
 
-import java.util.HashMap;
-
-import kr.ac.kpu.game.s2016182041.samplegame.ui.view.GameView;
 
 public class AnimationGameBitmap {
     private static final int PIXEL_MULTIPLIER = 4;
@@ -21,28 +16,21 @@ public class AnimationGameBitmap {
     private int frameIndex;
     private final float framesPerSecond;
     private final int frameCount;
-    private HashMap<Integer,Bitmap> bitmaps =new HashMap<Integer, Bitmap>();
-    public AnimationGameBitmap(int resId, float framesPerSecond, int frameCount){
-        Resources res = GameView.view.getResources();
-        BitmapFactory.Options opts =new BitmapFactory.Options();
-        opts.inScaled =false;
-        bitmap = bitmaps.get(resId); //한번이라도 불리면 키값 이용 다시사용
-        if(bitmap == null){
-            bitmap = BitmapFactory.decodeResource(res,resId,opts);
-            bitmaps.put(resId,bitmap);
-        }
+    public AnimationGameBitmap(int resId, float framesPerSecond, int frameCount) {
+        bitmap = GameBitmap.load(resId);
         imageWidth = bitmap.getWidth();
-        imageHeight= bitmap.getHeight();
-        if(frameCount==0){
-            frameCount=imageWidth/imageHeight ;
+        imageHeight = bitmap.getHeight();
+        if (frameCount == 0) {
+            frameCount = imageWidth / imageHeight;
         }
-        frameWidth = imageWidth/frameCount;
+        frameWidth = imageWidth / frameCount;
         this.framesPerSecond = framesPerSecond;
-        this.frameCount =frameCount;
+        this.frameCount = frameCount;
         createdOn = System.currentTimeMillis();
         frameIndex = 0;
     }
-//    public void update(){
+
+    //    public void update(){
 //        int elapsed =(int)(System.currentTimeMillis()- createdOn);
 //        frameIndex=Math.round(elapsed*0.001f*framesPerSecond)%24;
 //    }
