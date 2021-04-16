a368067811f88078dcad41db74b449a649141369
Jo <jkyu6554@kpu.ac.kr>
2021-04-16 10:39:05 +0900

    game view 교수님 수정

--- Programing/SampleGame/app/src/main/java/kr/ac/kpu/game/s2016182041/samplegame/ui/view/GameView.java
+++ Programing/SampleGame/app/src/main/java/kr/ac/kpu/game/s2016182041/samplegame/ui/view/GameView.java
@@ -23,7 +23,7 @@ public class GameView extends View {
     public GameView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
         GameView.view = this;
-        startUpdating();
+//        startUpdating();
     }
 
     @Override
@@ -31,7 +31,33 @@ public class GameView extends View {
         //super.onSizeChanged(w, h, oldw, oldh);
         Log.d(TAG, "onSize: " + w + "," + h);
         MainGame game = MainGame.get();
-        game.initResources();
+        boolean justInitialized = game.initResources();
+        if (justInitialized) {
+            requestCallback();
+        }
+    }
+
+    private void update() {
+        MainGame game = MainGame.get();
+        game.update();
+
+        invalidate();
+    }
+
+    private void requestCallback() {
+        Choreographer.getInstance().postFrameCallback(new Choreographer.FrameCallback() {
+            @Override
+            public void doFrame(long time) {
+                if (lastFrame == 0) {
+                    lastFrame = time;
+                }
+                MainGame game = MainGame.get();
+                game.frameTime = (float) (time - lastFrame) / 1_000_000_000;
+                update();
+                lastFrame = time;
+                requestCallback();
+            }
+        });
     }
 
     private void startUpdating() {
