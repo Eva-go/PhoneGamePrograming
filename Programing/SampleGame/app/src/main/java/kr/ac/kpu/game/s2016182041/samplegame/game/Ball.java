5956318b81dc95547e40bc72cf2e6160ccebd9c8
Jo <jkyu6554@kpu.ac.kr>
2021-04-16 10:44:14 +0900

    이미지를 각각 10개 돌릴때 사용법

--- Programing/SampleGame/app/src/main/java/kr/ac/kpu/game/s2016182041/samplegame/game/Ball.java
+++ Programing/SampleGame/app/src/main/java/kr/ac/kpu/game/s2016182041/samplegame/game/Ball.java
@@ -1,31 +1,28 @@
 package kr.ac.kpu.game.s2016182041.samplegame.game;
 
-import android.content.res.Resources;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
-import android.graphics.Rect;
-import android.graphics.RectF;
 
+import java.util.Random;
+
+import kr.ac.kpu.game.s2016182041.samplegame.R;
 import kr.ac.kpu.game.s2016182041.samplegame.framework.AnimationGameBitmap;
 import kr.ac.kpu.game.s2016182041.samplegame.framework.GameObject;
-import kr.ac.kpu.game.s2016182041.samplegame.R;
 import kr.ac.kpu.game.s2016182041.samplegame.ui.view.GameView;
 
 public class Ball implements GameObject {
     private float x, y;
     private float dx, dy;
-    private static AnimationGameBitmap bitmap;
+    private AnimationGameBitmap bitmap;
     private static float FRAME_RATE = 8.5f;
     public Ball(float x, float y, float dx, float dy) {
         this.x = x;
         this.y = y;
         this.dx = dx;
         this.dy = dy;
-        if (bitmap == null) {
-            bitmap=new AnimationGameBitmap(R.mipmap.fireball_128_24f,FRAME_RATE,0);
+        Random r = new Random();
+        float frameRate = FRAME_RATE * (r.nextFloat()* 0.4f+ 0.8f);
+        bitmap=new AnimationGameBitmap(R.mipmap.fireball_128_24f,frameRate,0);
         }
-    }
 
     public void update() {
         MainGame game = MainGame.get();
