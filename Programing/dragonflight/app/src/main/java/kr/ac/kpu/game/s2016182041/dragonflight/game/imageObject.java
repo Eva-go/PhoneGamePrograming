9e103ea91f8e1900ca342a0cb6aef8a163b5daa6
Jo <jkyu6554@kpu.ac.kr>
2021-05-07 11:22:54 +0900

    image object class 제작

---
+++ Programing/dragonflight/app/src/main/java/kr/ac/kpu/game/s2016182041/dragonflight/game/imageObject.java
@@ -0,0 +1,36 @@
+package kr.ac.kpu.game.s2016182041.dragonflight.game;
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Rect;
+import android.graphics.RectF;
+
+import kr.ac.kpu.game.s2016182041.dragonflight.framework.GameBitmap;
+import kr.ac.kpu.game.s2016182041.dragonflight.framework.GameObject;
+
+public class imageObject implements GameObject {
+    private final Bitmap bitmap;
+
+    private Rect srcRect = new Rect();
+    private RectF dstRect = new RectF();
+    public imageObject(int resId, float x, float y){
+        bitmap= GameBitmap.load(resId);
+        int w = bitmap.getWidth();
+        int h = bitmap.getHeight();
+        float l = x - w/2;
+        float t = y - h/2;
+        float r = x + w/2;
+        float b = y + h/2;
+        dstRect.set(l,t,r,b);
+        srcRect.set(0,0,w,h);
+    }
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+        canvas.drawBitmap(bitmap,srcRect,dstRect,null);
+    }
+}
