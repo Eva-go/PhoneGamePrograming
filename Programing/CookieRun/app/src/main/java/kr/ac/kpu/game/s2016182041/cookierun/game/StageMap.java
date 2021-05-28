4c8b3f39e50aaa72b7534fe3a2e5e86093a3da7d
Jo <jkyu6554@kpu.ac.kr>
2021-05-28 11:59:28 +0900

    이전버전 수정해결

--- Programing/CookieRun/app/src/main/java/kr/ac/kpu/game/s2016182041/cookierun/game/StageMap.java
+++ Programing/CookieRun/app/src/main/java/kr/ac/kpu/game/s2016182041/cookierun/game/StageMap.java
@@ -3,41 +3,39 @@ package kr.ac.kpu.game.s2016182041.cookierun.game;
 import android.content.res.AssetManager;
 import android.graphics.Canvas;
 import android.util.Log;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Random;
 
 import kr.ac.kpu.game.s2016182041.cookierun.framework.game.BaseGame;
 import kr.ac.kpu.game.s2016182041.cookierun.framework.iface.GameObject;
 import kr.ac.kpu.game.s2016182041.cookierun.framework.view.GameView;
-
 public class StageMap implements GameObject {
     private static final String TAG = StageMap.class.getSimpleName();
     private final ArrayList<String> lines = new ArrayList<String>();
-    private int column;
+    private int columns;
     private int rows;
     private int current;
 
-    public StageMap(String filename){
-       AssetManager assets = GameView.view.getContext().getAssets();
+    public StageMap(String filename) {
+        AssetManager assets = GameView.view.getContext().getAssets();
         try {
             InputStream is = assets.open(filename);
-            InputStreamReader isr = new InputStreamReader(is); //한바이트식
-            BufferedReader reader = new BufferedReader(isr); //한줄식
+            InputStreamReader isr = new InputStreamReader(is);
+            BufferedReader reader = new BufferedReader(isr);
             String header = reader.readLine();
             String[] comps = header.split(" ");
-            column = Integer.parseInt(comps[0]);
+            columns = Integer.parseInt(comps[0]);
             rows = Integer.parseInt(comps[1]);
-            Log.d(TAG,"col="+column+"row="+rows);
-            while (true){
+            Log.d(TAG, "Col=" + columns + " Row="  + rows);
+            while (true) {
                 String line = reader.readLine();
-                if(line == null){
+                if (line == null) {
                     break;
                 }
+                Log.d(TAG,  "-row=" + line);
                 lines.add(line);
             }
         } catch (IOException e) {
@@ -46,19 +44,18 @@ public class StageMap implements GameObject {
     }
     @Override
     public void update() {
-
         createColumn();
-
     }
-    private  float xPos;
+
+    private float xPos;
     private void createColumn() {
-        if(current>100){
+        if (current > 100) {
             return;
         }
         float y = 0;
-        for(int row = 0; row<rows; row++){
-            char ch = getAt(current,row);
-            createObject(ch, xPos,y);
+        for (int row = 0; row < rows; row++) {
+            char ch = getAt(current, row);
+            createObject(ch, xPos, y);
             y += Platform.UNIT_SIZE * GameView.MULTIPLIER;
         }
         current++;
@@ -67,47 +64,45 @@ public class StageMap implements GameObject {
 
     private void createObject(char ch, float x, float y) {
         MainGame game = (MainGame) BaseGame.get();
-        if(ch>='1' && ch <= '9'){
-            Jelly item = new Jelly(ch - '1',x,y);
-            game.add(MainGame.Layer.item,item);
-        } else if (ch >='0' && ch <= 'Q'){
-            Platform platform = new Platform(Platform.Type.values()[ch - '0'],x,y);
-            game.add(MainGame.Layer.platform,platform);
+        if (ch >= '1' && ch <= '9') {
+            Jelly item = new Jelly(ch - '1', x, y);
+            game.add(MainGame.Layer.item, item);
+        } else if (ch >= 'O' && ch <= 'Q') {
+            Platform platform = new Platform(Platform.Type.values()[ch - 'O'], x, y);
+            game.add(MainGame.Layer.platform, platform);
         }
     }
 
     private char getAt(int x, int y) {
-        try{
-            int lineIndex = x/ column *rows +y;
+        try {
+            int lineIndex = x / columns * rows + y;
             String line = lines.get(lineIndex);
-            return line.charAt(x%column);
-        } catch (Exception e){
+            return line.charAt(x % columns);
+        } catch (Exception e) {
             return 0;
         }
-
     }
-
-    /*ArrayList<GameObject> objects = game.objectsAt(MainGame.Layer.platform);
-        float rightMost = 0;
-        for(GameObject obj: objects) {
-            Platform platform = (Platform) obj;
-            float right = platform.getRight();
-            if (rightMost < right) {
-                rightMost = right;
-            }
-        }
-        float vw = GameView.view.getWidth();
-        float vh = GameView.view.getHeight();
-        if(rightMost < vw) {
-            Log.d(TAG, "create a Platform here !! @" + rightMost + " Platforms=" + objects.size());
-            float tx = rightMost, ty = vh - Platform.Type.T_2x2.height();
-            Platform platform = new Platform(Platform.Type.RANDOM, tx, ty);
-            game.add(MainGame.Layer.platform, platform);
-
-            Random r = new Random();
-            game.add(MainGame.Layer.item, new Jelly(r.nextInt(60), tx, r.nextInt((int) ty)));
-        }
-    }*/
+//        ArrayList<GameObject> objects = game.objectsAt(MainGame.Layer.platform);
+//        float rightMost = 0;
+//        for (GameObject obj: objects) {
+//            Platform platform = (Platform) obj;
+//            float right = platform.getRight();
+//            if (rightMost < right) {
+//                rightMost = right;
+//            }
+//        }
+//        float vw = GameView.view.getWidth();
+//        float vh = GameView.view.getHeight();
+//        if (rightMost < vw) {
+//            Log.d(TAG, "create a Platform here !! @" + rightMost + " Platforms=" + objects.size());
+//            float tx = rightMost, ty = vh - Platform.Type.T_2x2.height();
+//            Platform platform = new Platform(Platform.Type.RANDOM, tx, ty);
+//            game.add(MainGame.Layer.platform, platform);
+//
+//            Random r = new Random();
+//            game.add(MainGame.Layer.item, new Jelly(r.nextInt(60), tx, r.nextInt((int) ty)));
+//        }
+//    }
 
     @Override
     public void draw(Canvas canvas) {
